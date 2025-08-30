3c6b521eeafe70c287880e712a2b9082731aa3bd
Joey Frohlinger <jmafrohlinger@outlook.com>
2025-08-30 12:42:24 -0700

    starting to clean up and disentangle some likely flawed logic around tool handling. Works on Windows

--- ollama_llm.py
+++ ollama_llm.py
@@ -1,23 +1,32 @@
 from ollama import Client
+from ollama import _types
 from utilities import create_message
 import tools
 from weather_getter import get_forecast
 
+
+
 class OllamaLLM:
     system_prompt = create_message('system',"You are a helpful assistant who keeps things brief and responds only in english. Not every prompt requires a tool, if you don't think you need to use one - just respond normally as if you didn't have any provided.")
     initial_user_prompt = create_message('user','hello')
+
+    default_options = {'temperature':0.1}
+    default_tools = [
+            tools.list_files,
+            tools.get_now,
+            get_forecast
+        ]
 
     def __init__(self, model_string):
         self.model_name = model_string
         self.messages = [
             self.system_prompt, self.initial_user_prompt
         ]
-        self.tools = [
-            tools.list_files,
-            tools.get_now,
-            get_forecast
-        ]
+
         self.client = None
+        self.tools = self.default_tools
+        self.support_tools = True # Assume Tools are supported by default, if they aren't handle it gracefully later
+
 
     def __str__(self):
         return f"MyClass(param1={self.model_name})"
@@ -29,19 +38,16 @@ class OllamaLLM:
         self.client = Client(
             host='http://localhost:11434'  # talk to local ollama
         )
-        response = self.client.chat(model=self.model_name, messages=self.messages, options={'temperature':0.1})
+        response = self.client.chat(model=self.model_name, messages=self.messages)
         self.push_message('assistant', response.message.content)
         print(response.message.content)
 
     def chat(self, message):
-        self.push_message('user', message)
-        response = self.client.chat(model=self.model_name, messages=self.messages, tools=self.tools, options={'temperature':0.1})
-        if response.message.tool_calls and response.message.tool_calls[0].function.name != "do_nothing":
+        response = self.__send_message_to_llm(message=message)
+        if response.message.tool_calls:
             self.handle_tools(response.message.tool_calls)
             return
-        if response.message.tool_calls and response.message.tool_calls[0].function.name == "do_nothing":
-            response = self.client.chat(model=self.model_name, messages=self.messages, options={'temperature':0.1})
-        
+
         self.push_message('assistant', response.message.content)
         print(response.message.content)
 
@@ -57,3 +63,18 @@ class OllamaLLM:
                     response = self.client.chat(model=self.model_name, messages=self.messages, tools=self.tools)
                     self.push_message('assistant', response.message.content)
                     print(response.message.content)
+
+    def __send_message_to_llm(self, message, options=default_options):
+        self.push_message('user', message)
+
+        if self.support_tools:
+            try:
+                return self.client.chat(model=self.model_name, messages=self.messages, options=options, tools=self.tools)
+            except _types.ResponseError as e:
+                if e.error.__contains__("does not support tools"):
+                    self.support_tools = False # Set tool support to false and fall through
+                else: # Got some other type of error we haven't handled yet - spit it out and kill the process
+                    print(e.error)
+                    exit(-1)
+
+        return self.client.chat(model=self.model_name, messages=self.messages, options=options)
